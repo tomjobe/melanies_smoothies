76d9bf3ecf8ea2eb0fd3f8d3d16601fe61de953f
tomjobe <157994877+tomjobe@users.noreply.github.com>
2024-08-30 15:49:48 +0100

    restructured code order

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,17 +32,16 @@ ingredients_list = st.multiselect(
 if ingredients_list:
     ingredients_string = ''
 
-    search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-    st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-    
-    st.subheader(fruit_chosen + 'Nutritional Information:')
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
-    
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         
-        fv_df = st.dataframe(data = fruityvice_response.json(),use_container_width = True)
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        st.subheader(fruit_chosen + 'Nutritional Information:')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
         
+        fv_df = st.dataframe(data = fruityvice_response.json(),use_container_width = True)
+
     st.write(ingredients_string) 
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
